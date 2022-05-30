7ff45c6f7e6631915f62acb5a511119977d9a242
devbulygin <whoiswho68rus@gmail.com>
2022-05-30 22:16:03 +0300

    correct Engine.checkPrime

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -112,7 +112,7 @@ public class Engine {
         int temp;
         boolean isPrime = true;
 
-        for (int i = 2; i * i <= number; i++) {
+        for (int i = 1; i * i <= number; i++) {
             temp = number % i;
 
             if (temp == 0) {
